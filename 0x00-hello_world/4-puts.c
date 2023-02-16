82d399af16ae57f2c98b294b7e20139f6b156b6b
MR-AKER <rabbidose@gmail.com>
2023-02-16 08:39:42 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that uses put to print function
+ * Return: 0 (congrate)
+ */
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}

d1217b9ec855b1eef9c529becad7463a4dab0361
MR-AKER <rabbidose@gmail.com>
2023-02-16 09:01:40 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program the print a new line using the printf functin
+ * return: 0 (congrate)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

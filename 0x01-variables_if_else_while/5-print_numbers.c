00e71d2326fcf52e307a91705753eec2b6c76ec6
MR-AKER <rabbidose@gmail.com>
2023-02-17 16:16:44 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <std.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (succese)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		printf("%d", a)
+			putchar('\n');
+	return (0);
+}

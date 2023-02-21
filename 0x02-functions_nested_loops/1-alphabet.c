90ed734945fd45c42566c2bf3d150dfac6457b17
MR-AKER <rabbidose@gmail.com>
2023-02-21 12:54:01 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - make the alphabeth in lower case
+ * Return: void
+ */
+
+void Print_alphabet(void)
+{
+
+	char c;
+
+	for (c = 'a';c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - print all alphabeth in lower case
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+        char c;
+        int i = 0;
+
+        while (i <= 9)
+        {
+                for (c = 'a'; c <= 'z'; c++)
+                {
+        _putchar(c);
+                }
+
+        _putchar('\n');
+        i++
+        }
+}

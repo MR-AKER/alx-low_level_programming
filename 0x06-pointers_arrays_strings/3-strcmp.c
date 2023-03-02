dc33ea11a327e8cf8cf7a0160cd71b37f45e4ab0
MR-AKER <rabbidose@gmail.com>
2023-03-01 18:43:01 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,14 +2,21 @@
 #include <string.h>
 
 /**
- * _strcmp - a function that compares two strings
- * @s1: param1
- * @s2: param2
- * Return: a string
+ * _strcmp - a function that compares two strings.
+ * @s1: first param
+ * @s2: second param
+ * Return: if str1 < str2, the negative difference
+ * of the first unmatched character
+ * if str1 == str2, 0.
+ * if str1 > str2 the positive difference
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	strcmp (s1, s2);
-	return (s1);
+	while (*s1 < *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * reverse_array - a function that reverses the content of an array of integers
  * @a: array a
@@ -8,5 +8,10 @@
 
 void reverse_array(int *a, int n)
 {
+	int len;
 
+	for (len = n; len >= 0; len--)
+	{
+		printf("%d, ", a[len]);
+	}
 }

bacf5811f91469b7610c002d53d6cffd475b4cab
MR-AKER <rabbidose@gmail.com>
2023-03-03 08:45:40 -0800

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strstr - a function that locates a substring.
+ * @a: array
+ * Return: Always 0 (successs)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int l;
+
+	for (i = 0; l < 8; i++)
+	{
+		for (l = 0; l < 8; l++)
+			_putchar(a[i][l]);
+		_putchar('\n');
+}

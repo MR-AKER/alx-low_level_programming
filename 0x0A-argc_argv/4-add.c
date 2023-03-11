b28f5028cdb078c2223c4d7ddd751a3f21ffd6cc
MR-AKER <rabbidose@gmail.com>
2023-03-10 17:54:20 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - a program that prints its name
@@ -11,17 +12,18 @@ int main(int argc, char *argv[])
 {
 	int result = 0, num, i, j, k;
 
-	if (argc == 1)
-		printf("0\n");
-
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("%s\n", "Error");
+		if (argv[i][j] > '9' || argv[i][j] < '0')
+		{
+		printf("%s\n", "Error");
 			return (1);
 		}
 	}
+	}
+
 	for (k = 1; k < argc; k++)
 	{
 		num = atoi(argv[k]);

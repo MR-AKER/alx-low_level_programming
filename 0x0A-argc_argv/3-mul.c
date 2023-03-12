a89b28eb9cafbf2396543e95adb75aaa1c16747d
MR-AKER <rabbidose@gmail.com>
2023-03-11 20:08:10 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,12 +14,13 @@ int main(int argc, char **argv)
 
 	if (argc < 3)
 	{
-		printf("%s\n", "error");
+		printf( "Error\n");
 		return (1);
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 
 	printf("%d\n", num1 * num2);
+
 	return (0);
 }

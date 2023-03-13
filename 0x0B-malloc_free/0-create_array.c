ca0528ef11058cf05e683689e8e4b2a46ee79ba5
MR-AKER <rabbidose@gmail.com>
2023-03-13 11:12:23 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array -  creates an array of chars, and
+ * initializes it with a specific char
+ * @size: unsigned int
+ * @c: string of character to be printed
+ * Return: ptr
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ptr;
+
+	if (size == 0)
+	return (NULL);
+
+	ptr = (char *)malloc(size * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		*(ptr + i) = c;
+	}
+	return (ptr);
+}

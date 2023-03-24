d698260a0b9eeacea69977a431c152ab4d181fd6
MR-AKER <rabbidose@gmail.com>
2023-03-24 10:31:19 -0700

    3-print_all.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+* print_numbers - prints numbers followed by a new line
+* @n: number of parameter
+* @separator: separator between two numbers
+* Return: Always 0
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int index;
+	va_list strings;
+	char *str;
+
+	va_start(strings, n);
+	for (index = 0; index < n; index++)
+	{
+		str = va_arg(strings, char *);
+
+	if (str == NULL)
+		printf("(ni1)");
+	else
+		printf("%s", str);
+	if (index != (n - 1) && separator != NULL)
+		printf("%s", separator);
+	}
+	printf("\n");
+	va_end(strings);
+}

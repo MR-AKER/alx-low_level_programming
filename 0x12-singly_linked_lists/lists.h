e33bdd22228a07dbcd6c477ed4a0fcb4770a33ec
MR-AKER <rabbidose@gmail.com>
2023-03-30 10:06:41 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,20 @@
-#ifndef "LISTS_H"
-#define "LISTS_H"
+#ifndef LISTS_H
+#define LISTS_H
 
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length of the string
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ */
+
+typedef struct lists_s
+{
+	char *str;
+	unsigned int len;
+	struct lists_s *next;
+} list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
